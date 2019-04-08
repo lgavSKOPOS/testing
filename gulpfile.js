7eaf7005e220f4f82978c45692f571c9663a0549
Luke Gavin | SKOPOS CONNECT <luke.gavin@skopos-connect.de>
2019-04-08 17:24:48 +0200

    (-) tidied up gulpfile, removed watchify code

--- gulpfile.js
+++ gulpfile.js
@@ -13,28 +13,14 @@ var path = {
     pages: ["src/**/*.html"],
     typeScripts: ["src/app/**/*.ts"],
 };
-
 var fs = require("fs");
 var del = require("del");
-// var polyfill = require("@babel/polyfill")
 
-// import polyfill from "@babel/polyfill";
 gulp.task("copy-html", function() {
     return gulp.src(path.pages)
         .pipe(gulp.dest("dist"));
 });
 
-/* var typeScripts = gulp.src("./src/app/**.ts", "src/main.ts");
-
-var scripts = [];
-fs.readdir(path.app, function(err, list) {
-    list.forEach(function(value) {
-        scripts.concat(value);
-        console.log(value);
-    });
-});
-    console.log("Test: " + scripts + "\n");
-*/
 
 gulp.task("clean:frontend", function() {
     return del(`dist/**/*`, `!dist/TestAng/*`);
@@ -83,62 +69,3 @@ gulp.task("default", gulp.series("clean:frontend", gulp.parallel("copy-html"), f
 }));
 
 
-// Note: ---------------- Start Watch Browserify ----------------
-var watchedBrowserify = watchify(browserify({
-    basedir: ".",
-    cache: {},
-    debug: true,
-    entries: ["src/main.ts"],
-    packageCache: {},
-}).plugin(tsify)
-    .transform("babelify", {
-        babelrc: false,
-        extensions: [".ts"],
-        presets: [
-            [
-                "@babel/preset-env", {
-                targets: {
-                    browsers: [
-                        "ios > 8",
-                        "android > 4.2",
-                        "and_chr > 38",
-                        "and_ff > 56",
-                        "and_qq > 1.1",
-                        "and_uc > 9",
-                        "ie_mob > 10",
-                        "ie >= 7",
-                    ],
-                },
-                useBuiltIns: "entry",
-            },
-            ],
-        ],
-    }),
-);
-
-function bundle() {
-    return watchedBrowserify
-        .bundle()
-        .pipe(source("bundle.js"))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        // .pipe(uglify())
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest("dist"));
-}
-
-watchedBrowserify.on("update", bundle);
-watchedBrowserify.on("log", fancy_log);
-// Note: ---------------- End Watch Browserify ----------------
-
-
-
-// gulp.task("default", gulp.series(gulp.parallel("copy-html"), bundle));
-
-// var ts = require("gulp-typescript");
-// var tsProject = ts.createProject("tsconfig.json");
-// gulp.task("default", function () {
-//     return tsProject.src()
-//         .pipe(tsProject())
-//         .js.pipe(gulp.dest("dist"));
-// });
