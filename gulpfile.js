5d9de4e82e5ed356143392c80a301b7a5a6b1a93
Luke Gavin | SKOPOS CONNECT <luke.gavin@skopos-connect.de>
2019-04-10 14:59:15 +0200

    o changed gulpfile to run using child processes.exec

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
-var gulp = require("gulp");
+var babelify = require("babelify");
 var browserify = require("browserify");
-var source = require("vinyl-source-stream");
-var tsify = require("tsify");
+
+var exec = require("child_process").exec;
+var del = require("del");
+var fs = require("fs");
+
+var gulp = require("gulp");
 var uglify = require("gulp-uglify");
 var sourcemaps = require("gulp-sourcemaps");
-var buffer = require("vinyl-buffer");
-var watchify = require("watchify");
-// var fancy_log = require("fancy-log");
-
-var babelify = require("babelify");
-// var connect = require('gulp-connect');
-// var globby = require('globby');
-// var gutil = require('gulp-util');
+var watch = require("gulp-watch");
 var jshint = require("gulp-jshint");
+
+var tsify = require("tsify");
+
+var source = require("vinyl-source-stream");
+var buffer = require("vinyl-buffer");
+
+var watchify = require("watchify");
 
 var path = {
     app: "src/app/",
@@ -20,13 +24,13 @@ var path = {
     pages: ["src/**/*.html"],
     typeScripts: ["src/app/**/*.ts"],
 };
-var fs = require("fs");
-var del = require("del");
 
-gulp.task("copy-html", function() {
+gulp.task("copy", function() {
     return gulp.src([
         "src/**/*.html",
         "src/**/*.css",
+        "src/**/*.ico",
+
     ])
     .pipe(gulp.dest("dist"));
 });
@@ -50,25 +54,50 @@ gulp.task("browserify", () => {
         entries: ["src/main.ts"],
         packageCache: {},
     })
-        .plugin(tsify)
-        .transform("babelify", {
-            babelrc: false,
-            extensions: [".ts"],
-            presets: [
-                [
-                    "@babel/preset-env", {
+    .plugin(tsify)
+    .transform(babelify.configure({
+        babelrc: false,
+        extensions: [".ts"],
+        presets: [
+            [
+                "@babel/preset-env", {
+                    corejs: "2.6.5",
                     useBuiltIns: "entry",
-                },
-                ],
+            },
             ],
-        })
-        .bundle()
-        .pipe(source("bundle.js"))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        // .pipe(uglify())
-        .pipe(sourcemaps.write("./"))
-        .pipe(gulp.dest("dist"));
-})
+        ],
+    }))
+    .bundle()
+    .pipe(source("bundle.js"))
+    .pipe(buffer())
+    .pipe(sourcemaps.init())
+    // .pipe(uglify())
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("dist"));
+});
 
-gulp.task("default", gulp.series("clean:frontend", gulp.parallel("copy-html"), "browserify"));
+gulp.task("lite-server", function() {
+    exec("npm run dev-dist", function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+    });
+});
+
+gulp.task("watch", () => {
+     watch([
+            "main.ts",
+            "src/app/**/*.ts",
+            "index.html",
+            "src/app/**/*.html",
+            "gulpfile.js",
+         ],
+        gulp.series(
+            "clean:frontend",
+            "copy",
+            "browserify",
+            "lite-server",
+        ),
+     );
+});
+
+gulp.task("default", gulp.series("clean:frontend", gulp.parallel("copy"), "browserify", "lite-server", "watch"));
