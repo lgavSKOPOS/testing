4f621014550948b9d62925107bf9cc10ab0e0389
Luke Gavin | SKOPOS CONNECT <luke.gavin@skopos-connect.de>
2019-04-10 12:51:15 +0200

    + implemented new method of compilation using systemjs, it also does ot function yet

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,31 @@ var uglify = require("gulp-uglify");
 var sourcemaps = require("gulp-sourcemaps");
 var buffer = require("vinyl-buffer");
 var watchify = require("watchify");
-// var fancy_log = require("fancy-log");
+var jshint = require("gulp-jshint");
 
-var path = {
-    app: "src/app/",
-    pages: ["src/**/*.html"],
-    typeScripts: ["src/app/**/*.ts"],
-};
+var systemjsBuilder = require("gulp-systemjs-builder");
+var tsc = require("gulp-typescript");
+var tscConfig = require("./tsconfig.json");
+
+//NOTE: For Vendoring
+var concat = require("gulp-concat");
+
 var fs = require("fs");
 var del = require("del");
 
-gulp.task("copy-html", function() {
-    return gulp.src(path.pages)
+var path = {
+    app: "src/app/",
+    css: ["src/**/*.css"],
+    pages: ["src/**/*.html"],
+    typeScripts: ["src/app/**/*.ts"],
+};
+
+gulp.task("copy", function() {
+    return gulp.src([
+        "src/**/*.html",
+        "src/**/*.css",
+        "systemjs.config.js",
+    ])
         .pipe(gulp.dest("dist"));
 });
 
@@ -25,32 +38,46 @@ gulp.task("clean:frontend", function() {
     return del(`dist/**/*`, `!dist/TestAng/*`);
 });
 
-gulp.task("default", gulp.series("clean:frontend", gulp.parallel("copy-html"), function() {
+gulp.task("lint", function() {
+    return gulp.src("src/app/**/*.ts")
+        .pipe(jshint())
+        .pipe(jshint.reporter("jshint-stylish"))
+        .pipe(jshint.reporter("fail"));
+});
 
-    return browserify({
-        basedir: ".",
-        cache: {},
-        debug: true,
-        entries: ["src/main.ts"],
-        packageCache: {},
-    })
-    .plugin(tsify)
-        .transform("babelify", {
-            babelrc: false,
-            extensions: [".ts"],
-            presets: [
-                [
-                    "@babel/preset-env", {
-                        useBuiltIns: "entry",
-                    },
-                ],
-            ],
-        })
-    .bundle()
-    .pipe(source("bundle.js"))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    // .pipe(uglify())
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("dist"));
-}));
+gulp.task("compile"/*, gulp.series("lint")*/, function() {
+    return gulp.src("src/**/*.ts")
+        .pipe(sourcemaps.init())
+        .pipe(tsc(tscConfig.compilerOptions))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("dist/js"));
+});
+
+
+gulp.task("bundle:app", function() {
+    var builder = systemjsBuilder();
+    builder.loadConfigSync("systemjs.config.js");
+
+    builder.buildStatic("dist/js/main.js", "app.min.js", {})
+        .pipe(gulp.dest("dist/js"));
+});
+
+//NOTE: Start Vendoring
+
+var vendorJS = [
+    "node_modules/core-js/client/shim.min.js",
+    "node_modules/zone.js/dist/zone.js",
+    "node_modules/systemjs/dist/system.src.js",
+    "node_modules/jquery/dist/jquery.js",
+];
+
+gulp.task("vendor-js", function() {
+    return gulp.src(vendorJS, { allowEmpty: true })
+        .pipe(concat("vendor.js"))
+        .pipe(gulp.dest("dist/js"));
+});
+
+//NOTE: End Vendoring
+gulp.task("build", gulp.series("clean:frontend", "compile", "vendor-js", gulp.parallel("copy"), "bundle:app"));
+// gulp.task("run", gulp.series("build", "webserver"));
+gulp.task("default", gulp.series("build"));
--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
-// import  enableProdMode  from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import 'zone.js/dist/zone';
 import 'core-js/es5/index';
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 // Note: Polyfill entry point
-// import '@babel/polyfill';
+import '@babel/polyfill';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
-import { AppModule } from './app/app.module.js';
-import { environment } from './environments/environment.js';
-
-// if (environment.production) {
-//   enableProdMode();
-// }
-
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+if (environment.production) {
+    enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule);
+//# sourceMappingURL=main.js.map
